b701ea4a2d9cb370330b36566e6c113e7b9d8ced
artNehay <artemynehay@gmail.com>
2023-09-06 17:39:00 +0300

    Track TwoSum.kt

--- easy/TwoSum.kt
+++ easy/TwoSum.kt
@@ -1,7 +1,7 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+package easy
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+class Solution {
+    /*fun twoSum(nums: IntArray, target: Int): IntArray {
+
+    }*/
 }
