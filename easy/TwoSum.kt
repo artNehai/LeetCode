d6837afe08fc699d0676ea46b3a69434880845e8
artNehay <artemynehay@gmail.com>
2023-09-06 19:10:48 +0300

    Solve problem straightforward

--- easy/TwoSum.kt
+++ easy/TwoSum.kt
@@ -1,7 +1,22 @@
 package easy
 
-class Solution {
-    /*fun twoSum(nums: IntArray, target: Int): IntArray {
+// https://leetcode.com/problems/two-sum/
 
-    }*/
+class Solution {
+    fun twoSum(nums: IntArray, target: Int): IntArray {
+        for (i in 0..<nums.lastIndex) {
+            val element1 = nums[i]
+            for (j in i + 1..nums.lastIndex) {
+                val element2 = nums[j]
+                if (element1 + element2 == target) {
+                    return intArrayOf(i, j)
+                }
+            }
+        }
+        return intArrayOf()
+    }
+}
+
+fun main() {
+    println(Solution().twoSum(intArrayOf(3, 2, 4), 6).joinToString())
 }
