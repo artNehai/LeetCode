752fdbe1c442fea84f529d83d967e3ae5411fa94
artNehay <artemynehay@gmail.com>
2023-09-07 19:28:33 +0300

    Solve using Hash map

--- easy/TwoSum.kt
+++ easy/TwoSum.kt
@@ -1,22 +1,20 @@
 package easy
 
-// https://leetcode.com/problems/two-sum
-
 class Solution {
     fun twoSum(nums: IntArray, target: Int): IntArray {
-        for (i in 0..<nums.lastIndex) {
-            val element1 = nums[i]
-            for (j in i + 1..nums.lastIndex) {
-                val element2 = nums[j]
-                if (element1 + element2 == target) {
-                    return intArrayOf(i, j)
-                }
+        val numberToIndexMap = mutableMapOf<Int, Int>()
+        for (indexA in nums.indices) {
+            val a = nums[indexA]
+            val b = target - a
+            val indexB: Int? = numberToIndexMap[b]
+
+            if (indexB != null) {
+                return intArrayOf(indexA, indexB)
+            }
+            else {
+                numberToIndexMap[a] = indexA
             }
         }
         return intArrayOf()
     }
-}
-
-fun main() {
-    println(Solution().twoSum(intArrayOf(3, 2, 4), 6).joinToString())
 }
