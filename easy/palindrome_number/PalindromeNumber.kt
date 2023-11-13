cb61bba24e9d92dbb942cf7385c0d7ba4f79823f
artNehay <artemynehay@gmail.com>
2023-11-13 13:47:27 +0300

    Solve without using strings

--- easy/palindrome_number/PalindromeNumber.kt
+++ easy/palindrome_number/PalindromeNumber.kt
@@ -1,8 +1,8 @@
 package easy.palindrome_number
 
 class Solution {
-    fun isPalindrome(x: Int): Boolean {
-        val str = x.toString()
+    fun isPalindrome(number: Int): Boolean {
+        val str = number.toString()
         var left = 0
         var right = str.lastIndex
         repeat(str.length / 2) {
@@ -13,5 +13,33 @@ class Solution {
             right--
         }
         return true
+    }
+}
+
+class SolutionWithoutStrings {
+    fun isPalindrome(x: Int): Boolean {
+        if (x < 0) {
+            return false
+        }
+
+        var number = x
+        var reversed = 0
+        fun isNotHalved(): Boolean =
+            try {
+                number / reversed > 1
+            } catch (e: ArithmeticException) {
+                false
+            }
+
+        do {
+            val lastDigit = number % 10
+            reversed = (reversed * 10) + lastDigit
+            number /= 10
+        } while (isNotHalved())
+
+        if (reversed > number) {
+            reversed /= 10
+        }
+        return reversed == number
     }
 }
